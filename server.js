c8f6b416d0cfc3800df51545c01f52e76843b11f
federicomichela <federico.michela@gmail.com>
2018-02-19 23:05:51 +0000

    successfult deployment configuration

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
 app.use(serveStatic(__dirname + "/dist"));
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
